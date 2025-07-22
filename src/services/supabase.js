bad89d12a512097959e22ddb45ad1911e64f0e7c
vianziro <9649104+vianziro@users.noreply.github.com>
2025-07-22 19:41:46 +0700

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hcyexdbwkfeurrmdrysu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjeWV4ZGJ3a2ZldXJybWRyeXN1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTc3OTk3NiwiZXhwIjoyMDQ1MzU1OTc2fQ.DPhl7k0UhtV5M43pIkgS_KaINllHl4TiuGy8odY3NYc';
-
+const supabaseUrl = 'https://khrlinmbypabxdjyywld.supabase.co';
+// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjeWV4ZGJ3a2ZldXJybWRyeXN1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTc3OTk3NiwiZXhwIjoyMDQ1MzU1OTc2fQ.DPhl7k0UhtV5M43pIkgS_KaINllHl4TiuGy8odY3NYc';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtocmxpbm1ieXBhYnhkanl5d2xkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNDUxNDUsImV4cCI6MjA2ODcyMTE0NX0.zYLYW7K8nukPQLOgr1e-RUECN7cv7AdiMmIADWAGlTQ';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
